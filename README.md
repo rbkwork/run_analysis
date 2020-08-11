03ce2a2c56a0b2d884175c3ff5368880c804dd0c
rbkwork <rbkwork@users.noreply.github.com>
2020-08-11 17:41:08 -0400

    minor changes

--- README.md
+++ README.md
@@ -1,21 +1,13 @@
-# run_analysis
-Getting and Cleaning Data Course Project
+# Getting and Cleaning Data Course Project
 
 The data "Human Activity Recognition Using Smartphones Data Set" used in this assingment was taken from [here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
 
-
-It merges the training and the test sets to create one data set with their respective subject and activity labels.
-
-Extracts only the measurements on the mean and standard deviation for each measurement.
-
-Uses descriptive activity names to name the activities in the data set
-
-Appropriately labels the data set with descriptive variable names.
-
-And it creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-
-and then generates a tidy data text file that meets the principles of:
+*It merges the training and the test sets to create one data set with their respective subject and activity labels.
+*Extracts only the measurements on the mean and standard deviation for each measurement.
+*Uses descriptive activity names to name the activities in the data set
+*Appropriately labels the data set with descriptive variable names.
+*And it creates a second, independent tidy data set with the average of each variable for each activity and each subject, 
+**which meets the principles of:**
 
 1. Each variable forms a column.
 2. Each observation forms a row.
@@ -23,10 +15,12 @@ and then generates a tidy data text file that meets the principles of:
 
 from <http://vita.had.co.nz/papers/tidy-data.pdf>
 
+##Files explanation
+The file with the scrip and all the process is `run_analysis.R`
+In file `codebook.Rmd` is the Code Book that explains the variables in The tidy data set is in the `tidydata` file but it can be read automatially in your computer by entering the following script:
 
-The tidy dataset can be read
 ```{r}
-address <- "https://s3.amazonaws.com/coursera-uploads/user-longmysteriouscode/asst-3/massivelongcode.txt"
+address <- "https://raw.githubusercontent.com/rbkwork/run_analysis/master/tidydata"
 address <- sub("^https", "http", address)
 data <- read.table(url(address), header = TRUE)
 View(data)
@@ -34,4 +28,3 @@ View(data)
 
 
 
-There is an codebook with the specific description of the tidy data file contents with the units of eache
--- run_analysis.R
+++ run_analysis.R
@@ -3,35 +3,34 @@ library(tidyverse)
 library(reshape2)
 setwd("D:/R projects/coursera/run_analysis/UCI HAR Dataset (1)")
 
-temp <- list.files(pattern="*.txt",all.files = T) #take archive path
-temp_names<-sub(".txt","",temp)%>%tolower() #use paths to make names (all in lower-case)
-names(temp) <- basename(temp_names) #put corresponding names to the data so plyr makes columns names 
-data_info<- lapply(temp, fread) #it creates a list with all the data
+temp <- list.files(pattern="*.txt",all.files = T)    #take archive path
+temp_names<-sub(".txt","",temp)%>%tolower()            #use paths to make names (all in lower-case)
+names(temp) <- basename(temp_names)               #put corresponding names to the data so plyr makes columns names 
+data_info<- lapply(temp, fread)         #it creates a list with all the data
 
 ##1 Merges the training and the test sets to create one data set.
 data<-rbind(cbind(data_info$x_test,data_info$subject_test,data_info$y_test),cbind(data_info$x_train,data_info$subject_train,data_info$y_train)) 
 
 ##2 Extracts only the measurements on the mean and standard deviation for each measurement.
-names(data)<-c(1:561,"subject","activity") #it needs unique names
-features<-data_info[["features"]][["V2"]] #a table with the names
-data<-select(data,c("subject","activity"),grep("mean[/(][/)]|std[/(][/)]",features)) #take the names from the info to filter and filter the data
+names(data)<-c(1:561,"subject","activity")         #it needs unique names
+features<-data_info[["features"]][["V2"]]          #a table with the names
+data<-select(data,c("subject","activity"),grep("mean[/(][/)]|std[/(][/)]",features))       #take the names from the info to filter and filter the data
 
 ##3 Uses descriptive activity names to name the activities in the data set
-data<- merge(data_info[["activity_labels"]],data,by.x = "V1",by.y = "activity") # use the activity names stored in the file
-data<- data[,-1] #deletel the column wich was used to merge
+data<- merge(data_info[["activity_labels"]],data,by.x = "V1",by.y = "activity")           # use the activity names stored in the file
+data<- data[,-1]              #delete the column which was used to merge
 
 ##4 Appropriately labels the data set with descriptive variable names.
-names(data)<-c("activity","subject",grep("mean[/(][/)]|std[/(][/)]",features,value = T)) #name all the columns but they are not unique
+names(data)<-c("activity","subject",grep("mean[/(][/)]|std[/(][/)]",features,value = T))   #name all the columns but they are not unique
 
 
 ##5 From the data set in step 4, creates a second, independent tidy data set with the 
 ##  average of each variable for each activity and each subject
-data<-pivot_longer(data,3:68,"features") #gather the variables in a column
-tidy_data<- dcast(data,features+activity~subject,mean)
-tidy_data$subject<- as.numeric(tidy_data$subject)
-tidy_data<-pivot_longer(tidy_data,3:32,"subject")
-tidy_data<-data.table(tidy_data)
+data<-pivot_longer(data,3:68,"features")                     #gather the variables in a column
+tidy_data<- dcast(data,features+activity+subject~ .,mean)   #makes a table that obtains the mean value for each observation
+tidy_data<-rename(tidy_data, value = ".")        
+tidy_data$subject<- as.numeric(tidy_data$subject) 
 
 
-write.table(tidy_data,file="./tidydata")
+write.table(tidy_data,file="./tidydata") #make a file with the data table
 
